eaa76f46b8c65a2fcf6f5ae08c9a71a2ccfd1201
VictorLiubchenko <liubchenkovictor@gmail.com>
2020-07-12 16:29:26 +0300

    Update CarouselPageViewController.swift

--- blabla/blabla/CarouselPageViewController.swift
+++ blabla/blabla/CarouselPageViewController.swift
@@ -12,9 +12,9 @@ class CarouselPageViewController: UIPageViewController, CarouselItemDelegate {
     private var items: [UIViewController] = []
     weak var delegateController: CarouselPageViewControllerDelegate?
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         dataSource = self
         
         decoratePageControl()
@@ -26,20 +26,23 @@ class CarouselPageViewController: UIPageViewController, CarouselItemDelegate {
             setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
     }
+    
     func carouselView(view: CarouselItem, didTouchCompleteButton button: UIButton) {
         delegateController?.jumpToLogInPage()
     }
 }
 
 private extension CarouselPageViewController {
+    
     func decoratePageControl() {
+
         let pc = UIPageControl.appearance(whenContainedInInstancesOf: [CarouselPageViewController.self])
         pc.currentPageIndicatorTintColor = .systemPink
         pc.pageIndicatorTintColor = .gray
-
     }
     
     func populateItems(){
+        
         let textContent = ["WELCOME" , "ПРИВЕТ", "ALOHA"]
         let backgroundColor:[UIColor] = [.blue , .red , .magenta]
         
@@ -50,48 +53,55 @@ private extension CarouselPageViewController {
     }
     
     func createCarouselItemController(with titleText: String?, color: UIColor?) -> UIViewController{
+        
         let newViewControllet = UIViewController()
         var view: CarouselItem
         view = CarouselItem(titleText: titleText, background: color)
         view.delegate = self
+        
         newViewControllet.view = view
         return newViewControllet
     }
 }
     
-    //MARK: - DataSouse
-    
+//MARK: - DataSouse
 extension CarouselPageViewController: UIPageViewControllerDataSource {
         
     func pageViewController(_: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+       
         guard let viewControllerIndex = items.firstIndex(of: viewController) else {
-                return nil
-            }
+            return nil
+        }
             
         let previousIndex = viewControllerIndex - 1
             
         guard previousIndex >= 0 else {
-                return nil
+            return nil
         }
             
         guard items.count > previousIndex else {
-                return nil
+            return nil
         }
             
         return items[previousIndex]
     }
 
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+
         guard let viewControllerIndex = items.firstIndex(of: viewController) else {
-                return nil
+            return nil
         }
+        
         let nextIndex = viewControllerIndex + 1
+        
         guard items.count != nextIndex else {
             return nil
         }
+        
         guard items.count > nextIndex else {
-                return nil
+            return nil
         }
+        
         return items[nextIndex]
     }
         
@@ -100,13 +110,16 @@ extension CarouselPageViewController: UIPageViewControllerDataSource {
     }
         
     func presentationIndex(for _: UIPageViewController) -> Int {
+        
         guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = items.firstIndex(of: firstViewController) else {
             return 0
         }
+        
         return firstViewControllerIndex
     }
 }
 
 protocol CarouselPageViewControllerDelegate: class {
+    #warning("Need change function name. Minimum chabge "jump" to "go". Maximum this class protocol no nothing about login page, and function name should not contain words aboit it)")
     func jumpToLogInPage()
 }
